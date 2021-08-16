2fbe0536220fec90e95f7406f938ec525f7366e8
maim228 <72839974+maim228@users.noreply.github.com>
2021-08-16 11:58:30 +0200

    Update app.js
    add form validation with sweet alert

--- app.js
+++ app.js
@@ -5,20 +5,14 @@ document.getElementById('submit').addEventListener('click',function(event){
 
 function showS(){
     if(document.getElementById('name').value === ''){
-        document.getElementById('alert').style.display='block';
-        document.getElementById('alert-text').innerHTML='<i class="fa fa-times" aria-hidden="true"></i> Full Name is required';
-        setInterval(function(){ document.getElementById('alert').style.display='none'; }, 5000);
+        swal("Error!", "Full name is required!", "error");
     }else if(document.getElementById('email').value === ''){
-        document.getElementById('alert').style.display='block';
-        document.getElementById('alert-text').innerHTML='<i class="fa fa-times" aria-hidden="true"></i> Email is required';
-        setInterval(function(){ document.getElementById('alert').style.display='none'; }, 5000);
+        swal("Error!", "Email is required!", "error");
     }else if(document.getElementById('message').value === ''){
-        document.getElementById('alert').style.display='block';
-        document.getElementById('alert-text').innerHTML='<i class="fa fa-times" aria-hidden="true"></i> Your Message is required';
-        setInterval(function(){ document.getElementById('alert').style.display='none'; }, 5000);
+        swal("Error!", "hmmm! Forgot the message?", "error");
     }else{
-        document.getElementsByClassName('container')[0].style.display='none';
-        document.getElementById('success').style.display='block';
+        swal("Good job!", "Message Sent!", "success");
+        document.querySelector(".form").reset();
     }
 
 }
